a81425d43f7fa779c60b94fa1edaf5a1a139a8e6
Youngmin Kim <rladudals02@gmail.com>
2018-01-25 14:28:37 +0900

    ...

--- controllers/light/index.js
+++ controllers/light/index.js
@@ -3,54 +3,73 @@ const async = require("async");
 const config = require("config.json")("./config/config.json");
 const request = require("request");
 
+
 const constants = require('../../lib/constants');
 const makeGatewayURL = require('../../js/make_gateway_URL');
 
-var responseDemo = require('../../response/gateway/discovery.json');
-
-exports.discoverGateways = function(callback){
-  console.log("discoverGateway");
+exports.createLight = function(gatewayObject, lightId, callback){
+  console.log("createLight");
 
   var resultObject = {};
 
-  // TODO Add multicast request
-  // TEMP data
-
-
-  var response = responseDemo;
-
-  var gatewayObject = response.result_data;
-  gatewayObject.ip = config.sl.gw.ip;
-  gatewayObject.tcp_port = config.sl.gw.port;
-
-
-  var response = {};
-
-  if(gatewayObject === null){
-    response.type = constants.RESPONSE_SPEAK;
-    response.speechOutput = "Don't find the gateway...";
-  }else{
-    response.type = constants.RESPONSE_SPEAK_AND_LISTEN;
-    response.speechOutput = "Find the gateway, " + gatewayObject.gid + "!";
-    response.reprompt = 'What do you want to control the gateway?';
-  }
-
-  var data = {};
-
-  data.gateway = gatewayObject;
-  data.response = response;
 
   resultObject.code = constants.SL_API_SUCCESS_CODE;
   resultObject.message = "Success";
-  resultObject.data = data;
-
 
   callback(null, resultObject);
-};// discoverGateways
+};// createLight
+
+exports.loadLight = function(gatewayObject, lightId, callback){
+  console.log("loadLight");
+
+  var resultObject = {};
 
 
-exports.discoverLights = function(gatewayObject, callback){
-  console.log("discoverLights");
+  const ip = gatewayObject.ip;
+  const port = gatewayObject.tcp_port;
+  const version = config.sl.gw.version;
+
+  const gatewayURL = makeGatewayURL(ip, port, version);
+  var requestURL = gatewayURL + "/device/" + lightId;
+
+  var data = {
+    url: requestURL,
+    headers: {
+      'content-type': 'application/json'
+    }
+  }
+
+  request.get(data, function(error, httpResponse, body){
+    console.log(body);
+
+    var dataObject = JSON.parse(body);
+
+    var data = dataObject.result_data;
+
+    resultObject.code = constants.SL_API_SUCCESS_CODE;
+    resultObject.message = "Success";
+
+    resultObject.data = data;
+
+    callback(null, resultObject);
+  });
+};// loadLight
+
+
+exports.removeLight = function(gatewayObject, lightId, callback){
+  console.log("removeLight");
+
+  var resultObject = {};
+
+
+  resultObject.code = constants.SL_API_SUCCESS_CODE;
+  resultObject.message = "Success";
+
+  callback(null, resultObject);
+};// removeLight
+
+exports.loadLightList = function(gatewayObject, callback){
+  console.log("loadLightList");
 
   var resultObject = {};
 
@@ -96,7 +115,7 @@ exports.discoverLights = function(gatewayObject, callback){
       response.speechOutput = "Find the lights";
 
       for(let i = 0; i < lightList.length; i++){
-        response.speechOutput += ", " + lightList[i].did;
+        response.speechOutput += ", Light " + lightList[i].did;
       }
       response.speechOutput += "!";
 
@@ -123,4 +142,4 @@ exports.discoverLights = function(gatewayObject, callback){
 
     callback(null, resultObject);
   });
-}
+}//loadLightList
